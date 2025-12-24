e7ac8a069a99065ced2eb8b0a109be12cf27b3eb
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2025-12-24 19:59:40 +0530

    files structured

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./src/routes/auth.route.js"
+import messageRoutes from "./src/routes/message.route.js"
 dotenv.config(); 
 
 const app = express(); 
