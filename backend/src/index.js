85c522a8d71b4b68562c4d624857984cfe3f899f
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2025-12-25 21:51:04 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,27 @@ import express from 'express'
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-dotenv.config(); 
+import path from "path";
+dotenv.config();
 
-const app = express(); 
+const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 4044;
 
-app.use("/api/auth", authRoutes); 
-app.use("/api/message", messageRoutes); 
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-app.listen(PORT, ()=>{console.log(`server is running on port ${PORT}`)}); 
+
+// make ready for deployment 
+// setting in a way, where when we run the backend, our frontend will run auto inside the backend. 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("/*", (_, res) => {
+        res.send(path.join(__dirname, "../frontend/dist/index.html"))
+    });
+    // any route other than the defined above, will hit the *; 
+}
+
+app.listen(PORT, () => { console.log(`server is running on port ${PORT}`) }); 
