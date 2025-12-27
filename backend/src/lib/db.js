3752de5fdd5769ad82bf1666bab4371066be4e0b
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2025-12-27 21:13:18 +0530

    welcome email functionality added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"; 
-
+import { ENV } from "./env.js";
 export const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log("Mongo DB Connected", conn.connection.host); 
     } catch (error) {
         console.error("Error connection to MongoDB", error); 
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken"
-
+import { ENV } from "./env.js"
 export const generateToken = async(userId,res)=>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+    const token = jwt.sign({userId}, ENV.JWT_SECRET, {
         expiresIn: "7d" 
     })
     res.cookie("jwt", token, {
         maxAge: 7*24*60*60*100, // milisecs 7d
         httpOnly: true, // prevent XSS attacks
         sameSite: "strict", // prevent CSRF attacks
-        secure: process.env.NODE_ENV == "development" ? false : true
+        secure: ENV.NODE_ENV == "development" ? false : true
     })
 
     return token; 
