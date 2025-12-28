fa3f2a44328133dbc9ba6aa43a99a8a968ea29df
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2025-12-28 10:46:57 +0530

    implement security and rate limiting to the each auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controller/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router(); 
+
+router.use(arcjetProtection) // instead of writting it on each route, using this method, so that for each route first ajProtection is checked and then go to the next middlewares or controller. 
 
 router.post("/signup",signup)
 router.post("/login", login)
 router.post("/logout",logout)
+
 router.put("/update-profile", protectRoute, updateProfile)
+
 router.get("/check", protectRoute, (req,res)=>res.status(200).json(req.user));
 export default router; 
